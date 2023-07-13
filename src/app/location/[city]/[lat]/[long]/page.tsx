dda51bc22c75e1c8df4255126b4ff9f55c6bba13
nikul6 <nikuldhameliya6@gmail.com>
2023-07-13 13:28:18 +0530

    Humidity chart created

--- src/app/location/[city]/[lat]/[long]/page.tsx
+++ src/app/location/[city]/[lat]/[long]/page.tsx
@@ -6,6 +6,7 @@ import StatCard from '../../../../../../components/StatCard';
 import InformationPanel from '../../../../../../components/InformationPanel';
 import TempChart from '../../../../../../components/TempChart';
 import RainChart from '../../../../../../components/RainChart';
+import HumidityChart from '../../../../../../components/HumidityChart';
 
 type Props = {
   params: {
@@ -96,6 +97,7 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
         <div className='space-y-3'>
             <TempChart results={results}/>
             <RainChart results={results}/>
+            <HumidityChart results={results}/>
         </div>
       </div>
     </div>
